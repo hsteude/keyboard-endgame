cb717205b8fd77a2104121ef920d85bf12f75366
hsteude <henrik.steude@gmail.com>
2023-01-09 09:52:29 +0100

    Git repo reorga

--- compile.sh
+++ compile.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 node ~/ergogen/src/cli.js ergogen_file.yaml -o ergogen-output
-rm -r ./KiCadProject/henriks_planck.kicad_pcb
-cp ./ergogen-output/pcbs/henriks_planck.kicad_pcb ./KiCadProject
-
+cp ./ergogen-output/pcbs/henriks_planck.kicad_pcb ./KiCadProject/
